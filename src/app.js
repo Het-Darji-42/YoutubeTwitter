0dd758e99a8a41318aba130be3c193f14ed05939
Het-Darji-42 <hetdarji42@gmail.com>
2024-11-06 16:59:02 +0530

    App js changes + UTILS CODEs FOR HANDLINGS ERROR AND RESPONSE

--- src/app.js
+++ src/app.js
@@ -1,7 +1,49 @@
 import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser" 
 
 const app = express()
 
+//More Details At Last 
+
+    app.use(cors({                                   
+        origin : process.env.CORS_ORIGIN,         
+        credentials : true
+    }))  
+
+    app.use(express.json( {limit: "16kb"} ))
+
+    app.use(express.urlencoded( {extended: true, limit: "16kb"} ))
+
+    app.use(express.static("public")) 
 
 
 export {app}
+
+
+
+
+
+
+
+// = // app.use(cors())
+// app.use(cors({                                 // cors setting: FRONTEND THI KAI KAI JAGYA AE THI REQUEST ACCESS KARVANI CHHE   
+//     origin : process.env.CORS_ORIGIN,         // LINK FRONTEND NI 
+//     credentials : true
+// }))   
+
+
+// app.use(express.json(
+//     {limit: "16kb"}
+// ))
+
+
+// = //Url se data aye jem ke het+Darji || het%20Darji
+// app.use(express.urlencoded({
+//     extended: true,       // mOST OF TIME AA USE NAI THAY  [Use: object no object use karva ]
+//     limit: "16kb"
+// }))
+
+//=  //Koi image ke koi link avi and apde ane apda server ma store karva mangta hoiye to "PUBLIC FOLDER MA THAY "
+// app.use(express.static("public"))  
+
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,23 @@
+// Code 1 : (Most Of Used)  SMALL AND HARD
+
+const asyncHandler = (requestHandler) =>{}
+    	(req ,res , next)=>{
+            Promise.resolve(requestHandler(req ,res , next)).catch((err)=>{next(err)})
+        }
+export {asyncHandler}
+
+
+//  CODE 2 : ((LONG + TOO HARD))
+
+// // const asyncHandler = () => { async() => {} }   // Short Way OF THIS ==>
+//     const asyncHandler = (fn) => async(req , res , next ) =>{
+//         try {
+//             await fn(req , res , next)
+//         } catch (error) {
+//             res.status(error.code || 500).json({
+//                 success: false,
+//                 message: error.message 
+//             })
+//         }
+//     }
+// export {asyncHandler}
